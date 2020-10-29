deee66f3c31245511d200ff5c22fb58f441f7392
Andrii Otroshenko <andrii.otroshenko@gmail.com>
2020-10-29 15:13:44 +0200

    init: project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss'
+
+const App: React.FC = (): JSX.Element => {
+    return (
+        <div className="App">
+            <h1>Hello React!</h1>
+        </div>
+    )
+}
+
+export default App
